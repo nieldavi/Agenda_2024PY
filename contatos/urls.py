47cc8178941f5280e7fe0445df00f470d5b32894
Francisco Davi <niel.davi@hotmail.com>
2024-05-21 15:58:28 -0300

    buscar

--- contatos/urls.py
+++ contatos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from contatos.views import index, contato
+from contatos.views import index, contato, buscar
 
 urlpatterns = [
     path('', index),
-    path('contato/<int:info_id>',contato, name='contato')
+    path('contato/<int:info_id>',contato, name='contato'),
+    path('buscar', buscar, name='buscar')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -14,3 +14,11 @@ def contato(resquest,info_id):
     contato = get_object_or_404(Contato, pk =info_id)
     return render(resquest, 'contatos/det_contato.html', {'contato': contato})
 
+
+def buscar(request):
+    contatos = Contato.objects.all()
+    if 'buscar' in request.GET:
+        nome = request.GET['buscar']
+        if nome:
+            contatos = Contato.objects.filter(nome__icontains=nome)
+    return render(request, 'contatos/buscar.html', {'conts': contatos})
